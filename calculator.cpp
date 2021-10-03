bac1edf27323f864d8cb970f7c150bfad0adc95e
Carambula <antibog6@gmail.com>
2021-10-03 17:06:10 +0300

    little refactoring

--- calculator.cpp
+++ calculator.cpp
@@ -1,20 +1,11 @@
 #include "calculator.h"
 
-int Calculator::Add (double a, double b)
-{
+int Calculator::Add (double a, double b) { return a + b; }
 
-    return a + b;
+int Calculator::Sub (double a, double b) { return Add (a, -b); }
 
+int Calculator::Mul (double a, double b) { return a * b; }
 
-
-}
-
-int Calculator::Sub (double a, double b)
-{
-    return Add (a, -b);
-}
-
-int Calculator::Mul (double a, double b)
-{
-    return a * b + 0.5;
-}
+// todo something1
+// todo something2
+// todo something3
